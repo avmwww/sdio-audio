348355c99d0b8f36a380ed5b88cd48cbb73f4737
Andrey Mitrofanov <avm@mail.emb>
2013-05-17 18:06:28 +0400

    tdm frontend added

--- src/tb/data_gen.v
+++ src/tb/data_gen.v
@@ -5,7 +5,7 @@ module data_gen(
 	input  wire irst,              // reset
 	output wire [31: 0] och_data,  // channel data
 	output wire [ 3: 0] och_num,   // channel number
-	output wire och_data_valid     // valid data
+	output wire och_data_en     // valid data
 );
 
 endmodule
---
+++ src/tdm/tdm_fe.v
@@ -0,0 +1,13 @@
+// TDM front end
+
+module tdm_fe(
+	input  wire        iclk,
+	input  wire        irst,
+	input  wire [31:0] idata,	// Input data
+	input  wire        idata_en,	// Input data enable
+	input  wire        iout_data_dis,// disable output data
+	output wire [31:0] odata,	// Output data
+	output wire        odata_en,	// Output data enable
+);
+
+endmodule
