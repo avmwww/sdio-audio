5885464739249f548a8dda4a307ba76d87fc4ae2
Andrey Mitrofanov <avm@mail.emb>
2013-05-17 12:15:43 +0400

    verification module added

---
+++ src/tb/verif.v
@@ -0,0 +1,12 @@
+// Verification module
+
+
+module verif(
+	input wire imclk,  // Master clock input
+	input wire ifs,    // Frame sync
+	input wire isclk,  // Serial clock
+	input wire idin,   // Serial data input
+	output wire odout  // Serial data output
+);
+
+endmodule
