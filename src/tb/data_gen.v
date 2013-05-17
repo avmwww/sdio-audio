d8009b694970980b8b3ec45c6ac49abddbb0a541
MizantropSPb <mizantropspb@gmail.com>
2013-05-17 12:17:36 +0400

    Update data_gen.v

--- src/tb/data_gen.v
+++ src/tb/data_gen.v
@@ -1,8 +1,9 @@
 // Channel data generation module
 
 module data_gen(
-	input wire ireset,              // reset
-	input wire iclk,		// clock
+	input  wire iclk,	// clock
+	input  wire irst,       // reset
+
 	output wire [31: 0] och_data,   // channel data
 	output wire [ 3: 0] och_num,    // channel number
 	output wire och_data_valid      // valid data
