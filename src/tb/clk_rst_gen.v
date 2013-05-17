81375932ae62b78309642566de6a98acafa50903
Andrey Mitrofanov <avm@mail.emb>
2013-05-17 12:11:32 +0400

    test bench files added

---
+++ src/tb/clk_rst_gen.v
@@ -0,0 +1,9 @@
+// Clock generation module
+
+module clk_rst_gen(
+	output wire oclk, // generated lock
+	output wire orst  // generated reset
+);
+
+
+endmodule
---
+++ src/tb/data_gen.v
@@ -0,0 +1,10 @@
+// Channel data generation module
+
+module data_gen(
+	input wire ireset,             // reset
+	output wire [31: 0] och_data,  // channel data
+	output wire [ 3: 0] och_num,   // channel number
+	output wire och_data_valid     // valid data
+);
+
+endmodule
