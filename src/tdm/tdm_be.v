10ef2e8d3daa5e9490b381472167bed483c29c2f
Andrey Mitrofanov <avm@mail.emb>
2013-05-17 18:10:31 +0400

    tdm backend added

---
+++ src/tdm/tdm_be.v
@@ -0,0 +1,12 @@
+// TDM back end
+
+module tdm_be(
+	input  wire        iclk,
+	input  wire        irst,
+	input  wire [31:0] idin,	// Input data
+	output wire        odin_en,	// Input data enable
+	input  wire        idout_en,	// Output data enable
+	output wire [31:0] odout,	// Output data
+);
+
+endmodule
