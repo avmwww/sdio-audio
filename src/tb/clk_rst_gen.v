d083d88991ee493a90cc07d6dea95fe6ae4043a5
MizantropSPb <mizantropspb@gmail.com>
2013-05-21 17:18:12 +0400

    Update clk_rst_gen.v
    add clk_gen

--- src/tb/clk_rst_gen.v
+++ src/tb/clk_rst_gen.v
@@ -1,9 +1,20 @@
 // Clock generation module
+`timescale 1ns/1ps
 
-module clk_rst_gen(
-	output wire oclk, // generated lock
-	output wire orst  // generated reset
+module clk_rst_gen (
+    output reg          oclk,
+    output reg          orst
 );
 
+  initial begin
+      oclk = 1'h1;
+      forever oclk = #2 ~oclk;
+  end
+
+  initial begin
+      orst = 1'b1;
+      repeat (10) @(posedge oclk);
+      orst = 1'b0;
+  end
 
 endmodule
